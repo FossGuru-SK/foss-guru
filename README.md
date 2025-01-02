cf8e7f2627fbc2f9913b1b1c1840add2aacec5a3
SujitMBRDI <sujit.karne@mercedes-benz.com>
2025-01-02 16:38:05 +0530

    Deploy website - based on 2092e46367f71c24555d928ddf68b76cd3fb0c9b

--- README.md
+++
@@ -1,4 +0,0 @@
-
-# foss-guru
-
-A guide to using free open-source software in daily life, with a static website showcasing practical tips and solutions.
---
+++ assets/js/5e95c892.98200fa8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfoss_guru=self.webpackChunkfoss_guru||[]).push([[9647],{7121:(s,e,r)=>{r.r(e),r.d(e,{default:()=>l});r(6540);var u=r(4164),a=r(1213),c=r(7559),n=r(2831),t=r(781),f=r(4848);function l(s){return(0,f.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,f.jsx)(t.A,{children:(0,n.v)(s.route.routes)})})}}}]);
