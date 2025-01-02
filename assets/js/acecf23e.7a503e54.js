6573fa8a3b7519f9e739225bf4673074f77553e8
SujitMBRDI <sujit.karne@mercedes-benz.com>
2025-01-02 16:51:00 +0530

    Deploy website - based on 8904f71eb4f5e95dd3806a468d41cb63e4c42f18

---
+++ assets/js/acecf23e.7a503e54.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfoss_guru=self.webpackChunkfoss_guru||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
--- assets/js/acecf23e.c9eaffef.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfoss_guru=self.webpackChunkfoss_guru||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/foss-guru/blog","blogTitle":"Blog","authorsListPath":"/foss-guru/blog/authors"}')}}]);
